553d57f22326ce6c5a61794c42f264cb9c7fccba
Pranjal <65755220+reachpranjal@users.noreply.github.com>
2020-10-01 03:37:45 +0530

    Create indirect_recurr.cpp

---
+++ C++/Algorithms/RecursionAlgorithms/indirect_recurr.cpp
@@ -0,0 +1,27 @@
+// Indirecti Recursion to calculate factorial of a number
+
+#include <iostream>
+using namespace std;
+
+int func_a(int);
+int func_b(int);
+
+int func_a(int n){
+   if(n<=1)
+      return 1;
+   else
+      return n*func_b(n-1);
+}
+
+int func_b(int n){
+   if(n<=1)
+      return 1;
+   else
+      return n*func_a(n-1);
+}
+
+int main(){
+   int num = 5;
+   cout<< fa(num);
+   return 0;
+}
